d04e909710755e6c5e72971e1740e567ce9cb2cd
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-25 20:27:16 +0300

    Added 3 command to bot

--- yummy_bot.py
+++ yummy_bot.py
@@ -1,18 +1,34 @@
+import os
 import discord
+from discord.ext.commands import Bot
+from discord.ext.commands import Context
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    return
+yummy_bot = Bot(command_prefix="yummy:", intents=discord.Intents.all())
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@yummy_bot.command(name="hello")
+async def hello(ctx: Context):
+    """Command which reply "hello" to user"""
+    await ctx.channel.send("Hello " + ctx.author.name + "!")
 
-    await message.channel.send("ПЕРЕГУДИН ДОЛБАЕБ")
 
-client.run("ODM1NTM2MDIyMzA5MTc1MzU2.YIQ3jw.EAu7IPkLNLrt4z_E8kxnLmYYmaM");
+@yummy_bot.command(name="members")
+async def members(ctx: Context):
+    """Command which display a list of member's IDs and member's names"""
+    members_info = ""
+    for member in ctx.guild.members:
+        members_info += "ID: " + str(member.id) + "\tName: " + member.name + "\n"
+    await ctx.channel.send(members_info)
+
+
+@yummy_bot.command(name="repeat")
+async def repeat(ctx: Context, *messages: str):
+    """Command which repeat user's message"""
+    repeat_message = ""
+
+    for message in messages:
+        repeat_message += message + " "
+
+    await ctx.channel.send(repeat_message)
+
+yummy_bot.run(os.getenv("TOKEN"))
