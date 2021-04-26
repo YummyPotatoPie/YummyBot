2813ae7f053d375fc26770ee50e0658fc187ff26
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-26 16:26:02 +0300

    Added function which strign from strings tuple

--- src/BotUtilites.py
+++ src/BotUtilites.py
@@ -10,3 +10,10 @@ class BotUtilites:
     @staticmethod
     def check_ban_permission(ctx: Context) -> bool:
         return ctx.author.top_role.permissions.ban_members
+
+    @staticmethod
+    def string_list_to_string(*strings: [str, ...]):
+        result_string = ""
+        for i in range(len(strings[0])):
+            result_string += strings[0][i] + " "
+        return result_string
